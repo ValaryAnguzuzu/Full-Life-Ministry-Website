ac4930e5153daf616e4a88096032b47de4096edc
Valary Anguzuzu <anguzuzuvalary@gmail.com>
2025-02-08 20:53:28 +0300

    added Donate page

---
+++ full-life-ministry/src/pages/Donate.tsx
@@ -0,0 +1,14 @@
+const Donate = () => {
+  return (
+    <div className="container text-center">
+      <h2>Donate to Full Life Ministry</h2>
+      <p>
+        Your contributions help us spread the Word and support our community
+        programs.
+      </p>
+      <button className="btn btn-primary">Donate Now</button>
+    </div>
+  );
+};
+
+export default Donate;
