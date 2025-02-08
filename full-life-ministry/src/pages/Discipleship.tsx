aab1e553d9e39a4e55b1aadf302227f84cfcc6ec
Valary Anguzuzu <anguzuzuvalary@gmail.com>
2025-02-08 20:53:15 +0300

    Added descipleship page

---
+++ full-life-ministry/src/pages/Discipleship.tsx
@@ -0,0 +1,16 @@
+const Discipleship = () => {
+  return (
+    <div className="container">
+      <h2>Discipleship Classes</h2>
+      <p>Join our structured discipleship programs:</p>
+      <ul>
+        <li>New Life in Christ</li>
+        <li>Growing in Christ</li>
+        <li>Living in Christ</li>
+        <li>Evangelism in Christ</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Discipleship;
