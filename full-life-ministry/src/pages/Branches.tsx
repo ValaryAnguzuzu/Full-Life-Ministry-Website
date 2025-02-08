e05ba1271dcbb8f3564ba454952dac80632d4675
Valary Anguzuzu <anguzuzuvalary@gmail.com>
2025-02-08 20:53:01 +0300

    Added Branches page

---
+++ full-life-ministry/src/pages/Branches.tsx
@@ -0,0 +1,16 @@
+const Branches = () => {
+  return (
+    <div className="container">
+      <h2>Our Branches</h2>
+      <ul>
+        <li>Nairobi: Pastor James Mwangi</li>
+        <li>Mombasa: Pastor Sarah Ndungu</li>
+        <li>Kisumu: Pastor Peter Otieno</li>
+        <li>Nakuru: Pastor Emily Wanjiru</li>
+        <li>Eldoret: Pastor Daniel Kiptoo</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Branches;
