4f34f6d44e5ad07111e8576ed4caef693ebb1878
Valary Anguzuzu <anguzuzuvalary@gmail.com>
2025-02-08 20:53:48 +0300

    added Fellowships page

---
+++ full-life-ministry/src/pages/Fellowships.tsx
@@ -0,0 +1,17 @@
+const Fellowships = () => {
+  return (
+    <div className="container">
+      <h2>Home Fellowships</h2>
+      <ul>
+        <li>Bethlehem</li>
+        <li>Jerusalem</li>
+        <li>Zion</li>
+        <li>Judea</li>
+        <li>Samaria</li>
+        <li>Nazareth</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Fellowships;
