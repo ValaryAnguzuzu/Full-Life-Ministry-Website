4426e9fa6ceee717a3266859e67349b47025b9f6
Valary Anguzuzu <anguzuzuvalary@gmail.com>
2025-02-08 20:54:06 +0300

    added weekly schedule page

---
+++ full-life-ministry/src/pages/Schedule.tsx
@@ -0,0 +1,18 @@
+const Schedule = () => {
+  return (
+    <div className="container">
+      <h2>Weekly Schedule</h2>
+      <ul>
+        <li>Monday: Morning Glory (5AM - 6AM)</li>
+        <li>Tuesday: Prayer & Fasting, Sermon (4PM - 6PM)</li>
+        <li>Wednesday: Home Fellowships (4PM - 6PM)</li>
+        <li>Thursday: Bible Study</li>
+        <li>Friday: Discipleship Classes, Guidance & Counseling (4PM - 6PM)</li>
+        <li>Saturday: Praise & Worship Practice (4PM - 6PM)</li>
+        <li>Sunday: Service (9AM - 1PM)</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Schedule;
